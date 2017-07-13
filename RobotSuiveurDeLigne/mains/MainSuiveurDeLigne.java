16d8be68af1011138b8a8f0ee4ab9d6748f68383
Mélanie-Sama (Jessie) <melanie.cosmides@telecom-sudparis.eu>
2017-07-13 10:48:38 +0200

    add

---
+++ RobotSuiveurDeLigne/mains/MainSuiveurDeLigne.java
@@ -0,0 +1,26 @@
+package mains;
+
+import lejos.nxt.Motor;
+import lejos.nxt.NXTRegulatedMotor;
+import lejos.nxt.SensorPort;
+import lejos.nxt.UltrasonicSensor;
+import lejos.util.Delay;
+import tools.ChangeSquare;
+
+import java.util.ArrayList;
+
+import lejos.nxt.Button;
+import lejos.nxt.LCD;
+import lejos.nxt.LightSensor;
+
+
+
+public class MainSuiveurDeLigne {
+
+	public static void main(String[] args) {
+		
+		while(true){
+			
+		}
+	}
+}
