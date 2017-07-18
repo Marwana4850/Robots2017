0fefb5982838edc02135221b81bb4fd297aad03d
Mélanie-Sama (Jessie) <melanie.cosmides@telecom-sudparis.eu>
2017-07-18 16:48:25 +0200

    Code du Robot Suiveur de Lignes -- Version 1

--- RobotSuiveurDeLigne/mains/MainSuiveurDeLigne.java
+++ RobotSuiveurDeLigne/mains/MainSuiveurDeLigne.java
@@ -6,6 +6,7 @@ import lejos.nxt.SensorPort;
 import lejos.nxt.UltrasonicSensor;
 import lejos.util.Delay;
 import tools.ChangeSquare;
+import tools.ToolOne;
 
 import java.util.ArrayList;
 
@@ -19,7 +20,85 @@ public class MainSuiveurDeLigne {
 
 	public static void main(String[] args) {
 		
+		LightSensor lightG = new LightSensor(SensorPort.S4); //capteur lumineux gauche
+		LightSensor lightD = new LightSensor(SensorPort.S3); //capteur lumineux droit
+		Integer LGauche = lightG.getNormalizedLightValue();
+		Integer LDroite = lightD.getNormalizedLightValue();
+		Integer valeurSeuilNoir = 497;
+		
+		NXTRegulatedMotor motorG = Motor.C;
+		NXTRegulatedMotor motorD = Motor.B;
+		Integer VitesseMoteurs = 250;
+		Integer VitesseMoteurDroit = 200;
+		Integer VitesseMoteurGauche = 200;
+		Integer VitesseGauche = 0;
+		Integer VitesseDroite = 0;
+		
+		ToolOne PID1 = new ToolOne();
+		ToolOne PID2 = new ToolOne();
+		Integer EcartALaValeurNoire = 0;
+		Integer EcartARetenir = 0; //deviendra EcartPrecedent
+		Integer k = 1;
+		Integer valPID = 0;
+		
+		
 		while(true){
+			
+			LGauche = lightG.getNormalizedLightValue();
+			LDroite = lightD.getNormalizedLightValue();
+			motorG.setSpeed(VitesseMoteurs);
+			motorD.setSpeed(VitesseMoteurs);
+			motorG.forward();
+			motorD.forward();
+			
+			if(LGauche > valeurSeuilNoir && LDroite > valeurSeuilNoir){
+				
+				//aller tout droit
+				motorG.setSpeed(VitesseMoteurs);
+				motorD.setSpeed(VitesseMoteurs);	
+			}
+			else if(LDroite < valeurSeuilNoir){ //on voit du blanc à droite
+				
+				//tourner à gauche		
+				while(LDroite < valeurSeuilNoir){
+					//PID
+					EcartARetenir = PID1.getEcartALaValeurNoire(LDroite, valeurSeuilNoir);
+					
+					valPID = PID1.valeurPID();
+					PID1.setEcartPrecedent(EcartARetenir);
+					
+					//Vg > Vd
+					VitesseGauche = valPID*k + VitesseMoteurDroit;
+					motorD.setSpeed(VitesseMoteurDroit);
+					motorG.setSpeed(VitesseGauche);
+					
+					//on remesure
+					LDroite = lightD.getNormalizedLightValue();
+				}
+				PID1 = new ToolOne(); //on remet à 0 les valeurs
+			}
+			else if(LGauche < valeurSeuilNoir){ //on voit du blanc à gauche
+				
+				//tourner à droite
+				while(LGauche < valeurSeuilNoir){
+					//PID
+					EcartARetenir = PID2.getEcartALaValeurNoire(LGauche, valeurSeuilNoir);
+					
+					valPID = PID2.valeurPID();
+					PID2.setEcartPrecedent(EcartARetenir);
+					
+					//Vd > Vg
+					VitesseDroite = valPID*k + VitesseMoteurGauche;
+					motorD.setSpeed(VitesseDroite);
+					motorG.setSpeed(VitesseMoteurGauche);
+					
+					//on remesure
+					LGauche = lightG.getNormalizedLightValue();
+				}
+				PID2 = new ToolOne(); //on remet à 0 les valeurs
+			}
+			
+			
 			
 		}
 	}
--- RobotSuiveurDeLigne/tools/ToolOne.java
+++ RobotSuiveurDeLigne/tools/ToolOne.java
@@ -1,5 +1,49 @@
 package tools;
 
-public class ToolOne {
+import lejos.nxt.NXTRegulatedMotor;
 
+public class ToolOne {
+	
+	Integer EcartALaValeurNoire = 0;
+	Integer EcartPrecedent = EcartALaValeurNoire;
+	Integer valPID = 0;
+	Integer somme_erreur = 0;
+	Integer variation_erreur = 0;
+	Integer kp = 20;
+	Integer ki = 0;
+	Integer kd = 100;
+			
+	public ToolOne() {
+		this.EcartALaValeurNoire = 0;
+		this.EcartPrecedent = EcartALaValeurNoire;
+		this.valPID = 0;
+		this.somme_erreur = 0;
+		this.variation_erreur = 0;
+		this.kp = 20;
+		this.ki = 0;
+		this.kd = 100;
+	}
+	
+	public Integer getEcartALaValeurNoire(final int LDroiteOuGauche, final int valeurSeuilNoir){
+		if(LDroiteOuGauche >= valeurSeuilNoir){ //accessoire
+			this.EcartALaValeurNoire = 0;
+		}
+		else{
+			this.EcartALaValeurNoire = Math.abs(LDroiteOuGauche - valeurSeuilNoir);
+		}
+		return EcartALaValeurNoire;
+	}	
+	
+	public Integer valeurPID(){
+		somme_erreur += EcartALaValeurNoire;
+		variation_erreur = EcartALaValeurNoire - EcartPrecedent;
+		this.valPID = kp*EcartALaValeurNoire + ki*somme_erreur + kd*variation_erreur;
+		return valPID;
+		
+	}
+	
+	public void setEcartPrecedent(Integer EcartARetenir){
+		this.EcartPrecedent = EcartARetenir;
+	}
+	
 }
